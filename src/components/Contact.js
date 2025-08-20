fb137623c9c325c8e663e29365f6b3cbac0803e7
fred <fred0xx86@gmail.com>
2025-08-20 19:15:18 +0100

    Add content to Contact.js file

---
+++ src/components/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Find me on social media or reach out via email:</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/freddecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.github.com/fredDecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="mailto:fred0xx86@gmail.com">Email</a>
+        </li>
+      </ul>
+      <p>I look forward to connecting with you!</p>
+    </div>
+  );
+};
+
+export default Contact;
