3ae204ddac6ae6512732a826512ae448b58eaea6
fred <fred0xx86@gmail.com>
2025-08-20 15:47:53 +0100

    Add content to About.js file

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Frederick! I build modern web experiences and love to create
+        impactful software. This portfolio showcases my brand and tech skills.
+      </p>
+    </div>
+  );
+};
+
+export default About;
