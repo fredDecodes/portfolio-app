d8665de618502099d142f528425217aa74aa5edf
fred <86912882+fredDecodes@users.noreply.github.com>
2025-08-21 21:45:39 +0100

    Update viteConfig file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio-app/",
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+});
